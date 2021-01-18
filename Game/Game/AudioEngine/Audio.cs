6023b06ebf60d7760096d1b65e45f6ea31455392
Ross Hoyt <rosshoytmusic@gmail.com>
2021-01-18 12:25:07 -0800

    Set AudioClip volume in constructor, added doc

--- Game/Game/AudioEngine/Audio.cs
+++ Game/Game/AudioEngine/Audio.cs
@@ -17,71 +17,68 @@ namespace Game
         public static AudioClip MENU_CLICK_SOUND = new AudioClip("Game.AudioFiles.SFX.Menu.ButtonClickTest.wav");
     }
 
-    /// <summary>
-    /// Class that provides playback controls for a specified audio file
-    /// 
-    /// TODO add proper C# getter/setter syntax
-    /// see https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/object-and-collection-initializers
-    /// </summary>
-    class AudioClip
-    {
 
+    /// <summary>
+    /// Class which manages loading, setup, 
+    /// playback, and sound settings for an audio file player
+    /// </summary>
+    public class AudioClip //: AudioBus // TODO implement AudioBus
+    {
         /// <summary>
         /// Constructs an Audio Clip for a specified sound file with provided settings
-        ///
         /// </summary>
         /// <param name="filepath">
         /// Relative filepath of the sound file from the solution root.
-        /// String provided should have '/' replaced with  '.' for directories
+        /// String provided should have '/' replaced with  '.' for directories, 
+        /// e.g. "Game.AudioFiles.SFX.Menu.ButtonClickTest.wav"
         /// </param>
-        /// <param name="loop"></param>
-        public AudioClip(string filepath, bool loop = false)
+        /// <param name="loop">
+        /// (Default = false) Boolean value for whether this is a looping (repeating) audio clip
+        /// If false, it is a 'one-shot'
+        /// </param>
+        /// <param name="startingVolume">
+        /// (Default = 1.0) Floating point volume for the audio clip (range: 0.0 - 1.0)
+        /// </param>
+        /// TODO decide if AudioClip object should be valid if problem happens when loading file
+        public AudioClip(string filepath, bool loop = false, double startingVolume = 1.0)
         {
+            // Set class fields
             this.filepath = filepath;
+            // Setup sound player
             player = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
             player.Loop = loop;
-            Load();
+            player.Volume = startingVolume;
+            // Load the audio file as a stream, then load it into the audio player.
+            player.Load(Helpers.ResourceFileHelpers.GetStreamFromFile(filepath));
         }
         
+        /// <summary>
+        /// Plays the audio clip
+        /// </summary>
         public void Play()
         {
             player.Play();
         }
      
+        /// <summary>
+        /// Stops the audio clip
+        /// </summary>
         public void Stop()
         {
             player.Stop();
         }
-        private void Load()
-        {
-            player.Load(GetStreamFromFile(filepath));
-        }
-
 
         /// <summary>
-        /// Gets a stream for a resource file 
-        /// The file must have been added to visual studio and 
-        /// file properties set to resource file
+        /// The audio file player
         /// </summary>
-        /// <param name="filename"></param>
-        /// <returns></returns>
-        private Stream GetStreamFromFile(string filename)
-        {
-            var assembly = typeof(App).GetTypeInfo().Assembly;
-            var stream = assembly.GetManifestResourceStream(filename);
-            return stream;
-        }
+        private ISimpleAudioPlayer player;
 
         /// <summary>
-        /// the audio file path
+        /// The audio file's relative path from the solution folder
         /// </summary>
         private string filepath;
 
-        /// <summary>
-        /// the audio file's player
-        /// </summary>
-        private ISimpleAudioPlayer player;
+       
         
     }
-
 }
