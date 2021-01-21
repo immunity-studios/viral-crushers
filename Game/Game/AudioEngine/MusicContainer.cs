734b196d7689ae4b18867c40771717f3334043fc
Ross Hoyt <rosshoytmusic@gmail.com>
2021-01-20 16:33:50 -0800

    Implemented MusicContainer for multi-layer music pieces

--- Game/Game/AudioEngine/MusicContainer.cs
+++ Game/Game/AudioEngine/MusicContainer.cs
@@ -6,6 +6,19 @@ namespace Game.AudioEngine
 {
     public class MusicContainer
     {
+        public MusicContainer(List<AudioClip> layers)
+        {
+            this.layers = layers;
+        }
 
+        public void Play()
+        {
+            foreach (AudioClip clip in layers)
+            {
+                clip.Play();
+            }
+        }
+
+        private List<AudioClip> layers;
     }
 }
