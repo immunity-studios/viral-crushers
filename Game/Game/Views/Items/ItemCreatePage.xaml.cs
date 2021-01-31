1badd1b2a0e1e8e59541b9c9b97ad7e95f702251
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2021-01-30 19:58:50 -0800

    Fixed title on previous commit in the CharacterCreatePage.xaml.cs and ItemCreatePage.xaml.cs

--- Game/Game/Views/Items/ItemCreatePage.xaml.cs
+++ Game/Game/Views/Items/ItemCreatePage.xaml.cs
@@ -33,7 +33,7 @@ namespace Game.Views
 
             BindingContext = this.ViewModel;
 
-            this.ViewModel.Title = "Character Create";
+            this.ViewModel.Title = "Item Create";
 
             //Need to make the SelectedItem a string, so it can select the correct item.
             LocationPicker.SelectedItem = ViewModel.Data.Location.ToString();
