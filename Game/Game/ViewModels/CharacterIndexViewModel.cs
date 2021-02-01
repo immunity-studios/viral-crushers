7ac5caea7659a5d762717253de8d9595386e3013
Loi Pham <phaml5@seattleu.edu>
2021-01-31 23:44:08 -0800

    Renamed ItemUpdatePage to CharacterUpdatePage in CharacterIndexViewModel.cs

--- Game/Game/ViewModels/CharacterIndexViewModel.cs
+++ Game/Game/ViewModels/CharacterIndexViewModel.cs
@@ -64,7 +64,7 @@ namespace Game.ViewModels
             });
 
             // Register the Update Message
-            MessagingCenter.Subscribe<ItemUpdatePage, CharacterModel>(this, "Update", async (obj, data) =>
+            MessagingCenter.Subscribe<CharacterUpdatePage, CharacterModel>(this, "Update", async (obj, data) =>
             {
                 // Have the item update itself
                 data.Update(data);
