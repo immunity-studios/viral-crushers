ccc56fc433148b4f6573aead6b780684815168a5
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2021-02-06 08:41:33 -0800

    Removed Village and Game, and add Home in the hamburger menu in the MenuPage.xaml.cs

--- Game/Game/Views/MenuPage.xaml.cs
+++ Game/Game/Views/MenuPage.xaml.cs
@@ -32,9 +32,10 @@ namespace Game.Views
             // Establish the Menu List
             menuItems = new List<HomeMenuItemModel>
             {
-                new HomeMenuItemModel {Id = MenuItemEnum.Game, Title="Game" },
+                new HomeMenuItemModel {Id = MenuItemEnum.Home, Title="Home" },
+                //new HomeMenuItemModel {Id = MenuItemEnum.Game, Title="Game" },
                 new HomeMenuItemModel {Id = MenuItemEnum.About, Title="About" },
-                new HomeMenuItemModel {Id = MenuItemEnum.Village, Title="Village" },
+                //new HomeMenuItemModel {Id = MenuItemEnum.Village, Title="Village" },
                 new HomeMenuItemModel {Id = MenuItemEnum.Battle, Title="Battle" }
             };
 
