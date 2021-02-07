1ba76f9e47e8e8e247332ca43d20e1c003ebca2c
Loi Pham <phaml5@seattleu.edu>
2021-02-06 18:06:21 -0800

    Changed image source for the generic monster in MonsterModel.cs

--- Game/Game/Models/MonsterModel.cs
+++ Game/Game/Models/MonsterModel.cs
@@ -23,7 +23,7 @@ namespace Game.Models
             Attack = 1;
             Difficulty = DifficultyEnum.Average;
             UniqueItem = null;
-            ImageURI = "item.png";
+            ImageURI = "icon_red_monster.png";
             ExperienceTotal = 0;
             ExperienceRemaining = LevelTableHelper.LevelDetailsList[Level + 1].Experience - 1;
 
