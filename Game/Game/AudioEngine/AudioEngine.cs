9b47a4a7854148d64ea46c57eda81545dbf8abda
Ross Hoyt <rosshoytmusic@gmail.com>
2021-02-07 18:32:00 -0800

    Implemented Singleton pattern for Audio Engine, changed AudioEngine.cs

--- Game/Game/AudioEngine/AudioEngine.cs
+++ Game/Game/AudioEngine/AudioEngine.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
 using Xamarin.Forms;
 
 namespace Game.AudioEngine
 {
     public class AudioEngine
     {
+        #region Singleton
+
+        // Make this a singleton so it only exist one time because holds all the data records in memory
+        private static volatile AudioEngine instance;
+        private static readonly object syncRoot = new Object();
+
+        public static AudioEngine Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new AudioEngine();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        #endregion Singleton
+              
     }
 }
