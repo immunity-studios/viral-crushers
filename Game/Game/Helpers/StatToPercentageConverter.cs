8857565ee029188fc33cd5bdf8f930abaa8d8816
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2021-02-10 13:58:32 -0800

    Added a new class called StatToPercentageConverter.cs under the Helper folder to convert stats to percentage, in the project

---
+++ Game/Game/Helpers/StatToPercentageConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Game.Helpers
+{
+    public class StatToPercentageConverter : IValueConverter
+    {
+        /// <summary>
+        /// If value is an integer between 0 and 10, converts to a percentage
+        /// </summary>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int intValue && intValue >= 0 && intValue <= 10)
+            {
+                return intValue / 10f;
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
