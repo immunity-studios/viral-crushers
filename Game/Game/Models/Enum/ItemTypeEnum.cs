4d20dee0e1239a5f3393c7b69112c55f5c66e320
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2021-02-10 23:32:59 -0800

    Added helpers for consumable item types in ItemTypeEnum.cs

--- Game/Game/Models/Enum/ItemTypeEnum.cs
+++ Game/Game/Models/Enum/ItemTypeEnum.cs
@@ -44,5 +44,53 @@ namespace Game.Models
                 return myReturn;
             }
         }
+
+        /// <summary>
+        /// Returns true if the Item type is a consumable item type,
+        /// false otherwise.
+        /// </summary>
+        public static bool IsConsumableItemType(this ItemTypeEnum itemType)
+        {
+            switch (itemType)
+            {
+                case ItemTypeEnum.BoxOfTissues:
+                case ItemTypeEnum.Soup:
+                case ItemTypeEnum.BugSpray:
+                case ItemTypeEnum.Gloves:
+                case ItemTypeEnum.Vaccine:
+                case ItemTypeEnum.HazmatSuit:
+                case ItemTypeEnum.InvisibleCloak:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        /// <summary>
+        /// Returns the count for a consumable item type, or one if it is not
+        /// a consumable item type.
+        /// </summary>
+        public static int CountOfConsumable(this ItemTypeEnum itemType)
+        {
+            switch (itemType)
+            {
+                case ItemTypeEnum.BoxOfTissues:
+                    return 10;
+                case ItemTypeEnum.Soup:
+                    return 2;
+                case ItemTypeEnum.BugSpray:
+                    return 5;
+                case ItemTypeEnum.Gloves:
+                    return 4;
+                case ItemTypeEnum.Vaccine:
+                    return 1000;
+                case ItemTypeEnum.HazmatSuit:
+                    return 10;
+                case ItemTypeEnum.InvisibleCloak:
+                    return 10;
+                default:
+                    return 1;
+            }
+        }
     }
 }
