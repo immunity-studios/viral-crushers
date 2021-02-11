a70a6226f42bb7a626b533fa9e6f9358a6520d8f
Ross Hoyt <rosshoytmusic@gmail.com>
2021-02-10 20:01:15 -0800

    Added AudioEngineEventEnum.cs

---
+++ Game/Game/AudioEngine/AudioEngineEventEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.AudioEngine
+{
+    /// <summary>
+    /// Enum which contains all the supported events in the audio engine.
+    /// When the specific event occurs in code, pass the appropriate enum
+    /// into the AudioEngine.Instance.ProcessAudioEvent method
+    /// </summary>
+    public enum AudioEngineEventEnum
+    {
+
+    }
+}
