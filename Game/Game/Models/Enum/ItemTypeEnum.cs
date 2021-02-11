2ecb1fcd2aee511f872a55c55060772c9fc5058d
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2021-02-10 23:05:52 -0800

    Added a new Enum for item type called ItemTypeEnum in the project

---
+++ Game/Game/Models/Enum/ItemTypeEnum.cs
@@ -0,0 +1,22 @@
+﻿namespace Game.Models
+{
+    public enum ItemTypeEnum
+    {
+        Unknown = 0,
+        HandSoap = 2,
+        BoxOfTissues = 4,
+        Mask = 8,
+        Sanitizer = 16,
+        FaceShield = 32,
+        Soup = 64,
+        BugSpray = 128,
+        MosquitoNet = 256,
+        Goggles = 13,
+        Gloves = 25,
+        Vaccine = 20,
+        Medicine = 9,
+        HazmatSuit = 6,
+        Panacea = 5,
+        InvisibleCloak = 3
+    }
+}
