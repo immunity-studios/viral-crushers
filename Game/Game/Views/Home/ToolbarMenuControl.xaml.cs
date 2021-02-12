5b61db4a7ad58104d6a14e5493ccfa9941b669a6
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2021-02-12 12:11:49 -0800

    Added a ContentView form to make a user control for toolbar menu in the project

---
+++ Game/Game/Views/Home/ToolbarMenuControl.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Game.Views.Home
+{
+    public partial class ToolbarMenuControl : ContentView
+    {
+        public ToolbarMenuControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
