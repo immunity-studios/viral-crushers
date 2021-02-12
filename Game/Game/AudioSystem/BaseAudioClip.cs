b5a8935ddd6b78b48fa0d907847dca87efc0bafd
Ross Hoyt <rosshoytmusic@gmail.com>
2021-02-11 22:23:46 -0800

    Added BaseAudioClip.cs

---
+++ Game/Game/AudioSystem/BaseAudioClip.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.AudioSystem
+{   
+    /// <summary>
+    /// Partial implementation of the AudioClip interface contract
+    /// </summary>
+    public abstract class BaseAudioClip
+    {
+        public abstract bool Load();
+
+        public abstract bool Play();
+
+        /// <summary>
+        /// Flag which must be internally set to true if implementation of Load() succeeds when called
+        /// </summary>
+        public bool IsLoaded { get; private set; } = false;
+
+    }
+}
