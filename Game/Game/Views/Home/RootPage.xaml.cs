fce9be66896019587a9a73bc6bcda99e3cbc1aa2
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2021-02-12 10:17:59 -0800

    Added new Xamarin Forms file called RootPage in the project

---
+++ Game/Game/Views/Home/RootPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Game.Views.Home
+{
+    public partial class RootPage : ContentPage
+    {
+        public RootPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
