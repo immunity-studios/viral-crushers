22e850469b088ee9668a682bb5f23df9424a8470
Ross Hoyt <rosshoytmusic@gmail.com>
2021-02-12 17:37:55 -0800

    Added AudioBusEnum.cs

---
+++ Game/Game/AudioSystem/AudioBusEnum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.AudioSystem
+{
+    /// <summary>
+    /// Enum describing the audio summation busses used by sounds in the project.
+    /// </summary>
+    public enum AudioBusEnum
+    {
+        
+        Unknown = 0,
+
+        /// <summary>
+        /// The main output bus which all audio goes through
+        /// </summary>
+        Master = 1,
+
+        /// <summary>
+        /// Bus for all music 
+        /// </summary>
+        Music = 5,
+
+        /// <summary>
+        /// Bus for all sound effects
+        /// </summary>
+        SFX = 10
+    }
+}
