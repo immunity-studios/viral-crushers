8718c0dd544d4296e4fb93dd7206b74a7b347ba6
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2021-02-12 23:06:22 -0800

    Added toolbar menu user control to GamePage.xaml

--- Game/Game/Views/Home/GamePage.xaml.cs
+++ Game/Game/Views/Home/GamePage.xaml.cs
@@ -34,57 +34,12 @@ namespace Game.Views
 			await Navigation.PushAsync(new AboutPage());
 		}
 
-		#region Toolbar Click Handlers
-
 		/// <summary>
 		/// Jump to the Dungeon/Hospital
 		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="e"></param>
 		public async void DungeonButton_Clicked(object sender, EventArgs e)
 		{
 			await Navigation.PushAsync(new PickCharactersPage());
 		}
-
-		/// <summary>
-		/// Jump to the characters index page
-		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="e"></param>
-		public async void CharacterButton_Clicked(object sender, EventArgs e)
-		{
-			await Navigation.PushAsync(new CharacterIndexPage());
-		}
-
-		/// <summary>
-		/// Jump to the monsters index page
-		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="e"></param>
-		public async void MonsterButton_Clicked(object sender, EventArgs e)
-		{
-			await Navigation.PushAsync(new MonsterIndexPage());
-		}
-
-		/// <summary>
-		/// Jump to the items index page
-		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="e"></param>
-		public async void ItemButton_Clicked(object sender, EventArgs e)
-		{
-			await Navigation.PushAsync(new ItemIndexPage());
-		}
-
-		/// <summary>
-		/// Jump to the scores index page
-		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="e"></param>
-		public async void ScoreButton_Clicked(object sender, EventArgs e)
-		{
-			await Navigation.PushAsync(new ScoreIndexPage());
-		}
-		#endregion
 	}
 }
