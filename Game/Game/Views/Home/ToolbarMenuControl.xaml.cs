4f1f452c8b95684703d575c9c6e3d061fbe89ac1
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2021-02-12 23:01:36 -0800

    Added handlers for toolbar menu buttons in ToolbarMenuControl.xaml

--- Game/Game/Views/Home/ToolbarMenuControl.xaml.cs
+++ Game/Game/Views/Home/ToolbarMenuControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xamarin.Forms;
 
 namespace Game.Views
@@ -10,5 +11,73 @@ namespace Game.Views
         {
             InitializeComponent();
         }
-    }
+
+		#region Toolbar Click Handlers
+
+		/// <summary>
+		/// Jump to the Dungeon/Hospital
+		/// </summary>
+		public async void DungeonButton_Clicked(object sender, EventArgs e)
+		{
+			var previousPage = Navigation.NavigationStack.LastOrDefault();
+			await Navigation.PushAsync(new PickCharactersPage());
+			if (!(previousPage is GamePage))
+			{
+				Navigation.RemovePage(previousPage);
+			}
+		}
+
+		/// <summary>
+		/// Jump to the characters index page
+		/// </summary>
+		public async void CharacterButton_Clicked(object sender, EventArgs e)
+		{
+			var previousPage = Navigation.NavigationStack.LastOrDefault();
+			await Navigation.PushAsync(new CharacterIndexPage());
+			if (!(previousPage is GamePage))
+			{
+				Navigation.RemovePage(previousPage);
+			}
+		}
+
+		/// <summary>
+		/// Jump to the monsters index page
+		/// </summary>
+		public async void MonsterButton_Clicked(object sender, EventArgs e)
+		{
+			var previousPage = Navigation.NavigationStack.LastOrDefault();
+			await Navigation.PushAsync(new MonsterIndexPage());
+			if (!(previousPage is GamePage))
+			{
+				Navigation.RemovePage(previousPage);
+			}
+		}
+
+		/// <summary>
+		/// Jump to the items index page
+		/// </summary>
+		public async void ItemButton_Clicked(object sender, EventArgs e)
+		{
+			var previousPage = Navigation.NavigationStack.LastOrDefault();
+			await Navigation.PushAsync(new ItemIndexPage());
+			if (!(previousPage is GamePage))
+			{
+				Navigation.RemovePage(previousPage);
+			}
+		}
+
+		/// <summary>
+		/// Jump to the scores index page
+		/// </summary>
+		public async void ScoreButton_Clicked(object sender, EventArgs e)
+		{
+			var previousPage = Navigation.NavigationStack.LastOrDefault();
+			await Navigation.PushAsync(new ScoreIndexPage());
+			if (!(previousPage is GamePage))
+			{
+				Navigation.RemovePage(previousPage);
+			}
+		}
+		#endregion
+	}
 }
