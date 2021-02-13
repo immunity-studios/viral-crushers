9aefe10a97e8a57681445d5583dc4c398a5087c3
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2021-02-13 13:41:50 -0800

    Added a new folder for user controls in the project, and added a new control for curved labels in UserControls/CurvedLabelControl.xaml and CurvedLabelControl.xaml.cs

---
+++ Game/Game/Views/UserControls/CurvedLabelControl.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Game.Views
+{
+    public partial class CurvedLabelControl : ContentView
+    {
+        public CurvedLabelControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
