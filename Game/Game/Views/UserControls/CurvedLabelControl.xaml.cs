803897df6de09bc445b56c063587e28424defd36
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2021-02-13 17:29:43 -0800

    Added stylying and properties for curved label control in App.xaml, Game.csproj, CurvedLabelControl.xaml, CurvedLabelControl.xaml.cs

--- Game/Game/Views/UserControls/CurvedLabelControl.xaml.cs
+++ Game/Game/Views/UserControls/CurvedLabelControl.xaml.cs
@@ -10,5 +10,61 @@ namespace Game.Views
         {
             InitializeComponent();
         }
+
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(CurvedLabelControl));
+
+        public string Text
+        {
+            get
+            {
+                return (string)GetValue(TextProperty);
+            }
+            set
+            {
+                SetValue(TextProperty, value);
+            }
+        }
+
+        public static readonly BindableProperty TextBackgroundColorProperty = BindableProperty.Create(nameof(TextBackgroundColor), typeof(Color), typeof(CurvedLabelControl));
+
+        public Color TextBackgroundColor
+        {
+            get
+            {
+                return (Color)GetValue(TextBackgroundColorProperty);
+            }
+            set
+            {
+                SetValue(TextBackgroundColorProperty, value);
+            }
+        }
+
+        public static readonly BindableProperty CornerRadiusProperty = BindableProperty.Create(nameof(CornerRadius), typeof(float), typeof(CurvedLabelControl));
+
+        public float CornerRadius
+        {
+            get
+            {
+                return (float)GetValue(CornerRadiusProperty);
+            }
+            set
+            {
+                SetValue(CornerRadiusProperty, value);
+            }
+        }
+
+        public static readonly BindableProperty FontSizeProperty = BindableProperty.Create(nameof(FontSize), typeof(double), typeof(CurvedLabelControl));
+
+        public double FontSize
+        {
+            get
+            {
+                return (double)GetValue(FontSizeProperty);
+            }
+            set
+            {
+                SetValue(FontSizeProperty, value);
+            }
+        }
     }
 }
