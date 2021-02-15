2d1dfb23bf3ec33599f998af918500247860b90d
Ross Hoyt <rosshoytmusic@gmail.com>
2021-02-14 23:56:48 -0800

    Added doc to MusicContainer.cs

--- Game/Game/AudioSystem/MusicContainer.cs
+++ Game/Game/AudioSystem/MusicContainer.cs
@@ -4,13 +4,25 @@ using System.Text;
 
 namespace Game.AudioSystem
 {
+    /// <summary>
+    /// Class which manages playback for a multi-layer, multi-audio file song.
+    /// </summary>
     public class MusicContainer
     {
+        /// <summary>
+        /// Constructor that creates a music containers using provided audio layers.
+        /// </summary>
+        /// <param name="layers">
+        /// A list of looping audio files that should be played in sync.
+        /// </param>
         public MusicContainer(List<AudioClip> layers)
         {
             this.layers = layers;
         }
 
+        /// <summary>
+        /// Starts playback for the music container.
+        /// </summary>
         public void Play()
         {
             foreach (AudioClip clip in layers)
@@ -19,6 +31,13 @@ namespace Game.AudioSystem
             }
         }
 
+        /// <summary>
+        /// List of audio clips that play in sync.
+        /// </summary>
         private List<AudioClip> layers;
+
+        // TODO
+        // public void Stop();
+        // public void SetLayerVolume(int layer);
     }
 }
