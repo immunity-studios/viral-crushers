f887d4f7e1d172da02b4188017a3ab105b1bcc17
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2021-02-15 14:16:23 -0800

    Added a new Back button User Control in project

---
+++ Game/Game/Views/UserControls/BackButtonControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Game.Views
+{
+    public partial class BackButtonControl : ImageButton
+    {
+        public BackButtonControl()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty PopModalProperty = BindableProperty.Create(nameof(PopModal), typeof(bool), typeof(CurvedLabelControl));
+
+        public bool PopModal
+        {
+            get
+            {
+                return (bool)GetValue(PopModalProperty);
+            }
+            set
+            {
+                SetValue(PopModalProperty, value);
+            }
+        }
+
+        async void BackButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            if (PopModal)
+            {
+                await Navigation.PopModalAsync();
+            }
+            else
+            {
+                await Navigation.PopAsync();
+            }
+        }
+    }
+}
