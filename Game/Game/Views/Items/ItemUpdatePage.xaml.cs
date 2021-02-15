45e3a0d7c02ca922111023491b94abfe75d4f165
Loi Pham <phaml5@seattleu.edu>
2021-02-15 00:27:03 -0800

    Updated UI of Item Update Page. Changed in ItemUpdatePage.xaml

--- Game/Game/Views/Items/ItemUpdatePage.xaml.cs
+++ Game/Game/Views/Items/ItemUpdatePage.xaml.cs
@@ -73,24 +73,24 @@ namespace Game.Views
             await Navigation.PopModalAsync();
         }
 
-        /// <summary>
-        /// Catch the change to the Stepper for Range
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void Range_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
-        {
-            RangeValue.Text = String.Format("{0}", e.NewValue);
-        }
+        ///// <summary>
+        ///// Catch the change to the Stepper for Range
+        ///// </summary>
+        ///// <param name="sender"></param>
+        ///// <param name="e"></param>
+        //public void Range_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        //{
+        //    RangeValue.Text = String.Format("{0}", e.NewValue);
+        //}
 
-        /// <summary>
-        /// Catch the change to the stepper for Value
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        public void Value_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
-        {
-            ValueValue.Text = String.Format("{0}", e.NewValue);
-        }
+        ///// <summary>
+        ///// Catch the change to the stepper for Value
+        ///// </summary>
+        ///// <param name="sender"></param>
+        ///// <param name="e"></param>
+        //public void Value_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        //{
+        //    ValueValue.Text = String.Format("{0}", e.NewValue);
+        //}
     }
 }
