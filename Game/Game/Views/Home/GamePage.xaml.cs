bc6baa55d9a260655071a00436b02cb31cbe0822
Ross Hoyt <rosshoytmusic@gmail.com>
2021-02-17 00:07:31 -0800

    Added call to AudioEngine.ProcessAudioEvent method to update Audio Engine that player has entered dungeon, changed GamePage.xaml.cs

--- Game/Game/Views/Home/GamePage.xaml.cs
+++ Game/Game/Views/Home/GamePage.xaml.cs
@@ -40,6 +40,8 @@ namespace Game.Views
 		public async void DungeonButton_Clicked(object sender, EventArgs e)
 		{
 			await Navigation.PushAsync(new PickCharactersPage());
+			// Update Audio Engine the battle has started
+			AudioSystem.AudioEngine.Instance.ProcessAudioEvent(AudioSystem.AudioEventEnum.BattleStart);
 		}
 	}
 }
