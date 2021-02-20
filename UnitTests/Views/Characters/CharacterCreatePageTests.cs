91574f9987444c78acdc525ae0b469c75fcd28a0
lmirzaei <41973606+lmirzaei@users.noreply.github.com>
2021-02-19 22:18:35 -0800

    Added five UT classes for Character CRUDi pages in UnitTests.csproj

---
+++ UnitTests/Views/Characters/CharacterCreatePageTests.cs
@@ -0,0 +1,111 @@
+﻿using NUnit.Framework;
+
+using Game;
+using Game.Views;
+using Game.ViewModels;
+using Game.Models;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Mocks;
+
+namespace UnitTests.Views
+{
+    [TestFixture]
+    public class CharacterCreatePageTests : CharacterCreatePage
+    {
+        App app;
+        CharacterCreatePage page;
+
+        public CharacterCreatePageTests() : base(true) { }
+        
+        [SetUp]
+        public void Setup()
+        {
+            // Initialize Xamarin Forms
+            MockForms.Init();
+
+            //This is your App.xaml and App.xaml.cs, which can have resources, etc.
+            app = new App();
+            Application.Current = app;
+
+            page = new CharacterCreatePage();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Application.Current = null;
+        }
+
+        [Test]
+        public void CharacterCreatePage_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = page;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void CharacterCreatePage_Cancel_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.Cancel_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
+        public void CharacterCreatePage_Save_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.Save_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
+        public void CharacterCreatePage_Save_Clicked_Null_Image_Should_Pass()
+        {
+            // Arrange
+            page.ViewModel.Data.ImageURI = null;
+
+            // Act
+            page.Save_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
+        public void CharacterCreatePage_OnBackButtonPressed_Valid_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            OnBackButtonPressed();
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+    }
+}
