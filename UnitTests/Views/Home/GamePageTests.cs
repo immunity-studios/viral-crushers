57499a2bd9f1142e04cfe2ad25f4dcbdbf600dd1
Phong Nguyen <nguyenphong@seattleu.edu>
2021-02-22 02:02:58 -0800

    Updated Views folder in Unit Tests from Mike

--- UnitTests/Views/Home/GamePageTests.cs
+++ UnitTests/Views/Home/GamePageTests.cs
@@ -69,6 +69,19 @@ namespace UnitTests.Views
         }
 
         [Test]
+        public void GamePage_VillageButton_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+            // Act
+            page.VillageButton_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
         public void GamePage_AutobattleButton_Clicked_Default_Should_Pass()
         {
             // Arrange
