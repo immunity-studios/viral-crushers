7a1ce1d979d8cf47000d1b604e5f5ae66e29aed7
Ross Hoyt <43669429+rosshoyt@users.noreply.github.com>
2021-02-21 21:54:00 -0800

    Set default starting volume to 0, added GetBusVolume method to AudioEngine.cs

--- Game/Game/AudioSystem/AudioEngine.cs
+++ Game/Game/AudioSystem/AudioEngine.cs
@@ -47,6 +47,13 @@ namespace Game.AudioSystem
         public AudioEngine()
         {
             audioResources = new AudioResources();
+            busVolumes = new Dictionary<AudioBusEnum, double>();
+            // Set starting volume of all audio buses to 0
+            double startingVolume = 0;
+            foreach(AudioBusEnum bus in Enum.GetValues(typeof(AudioBusEnum)))
+            {
+                busVolumes.Add(bus, startingVolume);
+            }
         }
 
         public bool LoadAudio()
@@ -56,9 +63,8 @@ namespace Game.AudioSystem
                 bool loaded = audioClip.Load();
                 //Console.WriteLine(loaded? "Loaded" : "Failed to load" + " audio file with path " + audioClip.Filepath);
             }
-
+            SetBusVolume(AudioBusEnum.Master, 0);
             return true;
-
         }
 
         /// <summary>
@@ -78,13 +84,16 @@ namespace Game.AudioSystem
                 case AudioEventEnum.Button_GameStart:
                     audioResources.MENU_CLICK_SOUND.Play();
                     break;
+
                 case AudioEventEnum.MenuStart:
                     audioResources.MX_MENU_FULL.Play(); 
                     break;
+                
                 case AudioEventEnum.BattleStart:
                     audioResources.MX_MENU_FULL.Stop();
                     audioResources.MX_BATTLE_FULL.Play();
                     break;
+                
                 default:
                     return false; // event was not found, so return false
             }
@@ -116,6 +125,26 @@ namespace Game.AudioSystem
             return true;
         }
 
+        /// <summary>
+        /// Method that Returns the current volume of an audio bus
+        /// </summary>
+        /// <param name="bus">
+        /// The bus to get the volume of
+        /// </param>
+        /// <returns>
+        /// The current volume of the specified audio bus
+        /// </returns>
+        public double GetBusVolume(AudioBusEnum bus)
+        {
+            return busVolumes[bus];
+        }
+
         private AudioResources audioResources;
+
+        /// <summary>
+        /// Dictionary containing the volumes of the audio buses.
+        /// Is set-up in the AudioEngine constructor
+        /// </summary>
+        private Dictionary<AudioBusEnum, double> busVolumes;
     }
 }
