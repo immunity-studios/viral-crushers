5dba9f731416b36d2c9d637b8ebe193dcaa83b41
Phong Nguyen <nguyenphong@seattleu.edu>
2021-02-22 19:46:50 -0800

    Updated MonsterModel GetDamageRollAttack Default Speed in MonsterModelTests.cs

--- UnitTests/Models/MonsterModelTests.cs
+++ UnitTests/Models/MonsterModelTests.cs
@@ -750,7 +750,7 @@ namespace UnitTests.Models
             Game.Helpers.DiceHelper.DisableForcedRolls();
  
             // Assert
-            Assert.AreEqual(2, result);
+            Assert.AreEqual(1, result);
         }
 
         [Test]
