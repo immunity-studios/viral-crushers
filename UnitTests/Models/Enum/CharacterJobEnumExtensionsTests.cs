5b09eb74a4883f37289e1a41565c42ed2c92487e
Phong Nguyen <nguyenphong@seattleu.edu>
2021-02-22 22:02:37 -0800

    Added tests in CharacterJobEnumExtensionsTests.cs

--- UnitTests/Models/Enum/CharacterJobEnumExtensionsTests.cs
+++ UnitTests/Models/Enum/CharacterJobEnumExtensionsTests.cs
@@ -48,5 +48,61 @@ namespace UnitTests.Models
             // Assert
             Assert.AreEqual("Teacher", result);
         }
+
+        [Test]
+        public void CharacterJobEnumExtensionsTests_Doctor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = CharacterJobEnum.Doctor.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Doctor", result);
+        }
+
+        [Test]
+        public void CharacterJobEnumExtensionsTests_Athlete_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = CharacterJobEnum.Athlete.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Athlete", result);
+        }
+
+        [Test]
+        public void CharacterJobEnumExtensionsTests_PoliceOfficer_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = CharacterJobEnum.PoliceOfficer.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Police Officer", result);
+        }
+
+        [Test]
+        public void CharacterJobEnumExtensionsTests_CollegeStudent_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = CharacterJobEnum.CollegeStudent.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("College Student", result);
+        }
     }
 }
