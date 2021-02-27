49c97aeb2cf63014db1a268a650c70d5c0a381ea
Phong Nguyen <nguyenphong@seattleu.edu>
2021-02-26 23:00:17 -0800

    Updated DiceHelper.cs from Mike

--- Game/Game/Helpers/DiceHelper.cs
+++ Game/Game/Helpers/DiceHelper.cs
@@ -2,6 +2,9 @@
 
 namespace Game.Helpers
 {
+    /// <summary>
+    /// Helper to roll dice and random numbers
+    /// </summary>
     public static class DiceHelper
     {
         /// <summary>
@@ -17,20 +20,29 @@ namespace Game.Helpers
 
         // Holds the random value for the sytem
         private static int _ForcedRandomValue = 1;
-
-        // What number should return for random numbers (1 is good choice...)
-        public static void SetForcedRollValue(int value)
+        
+        /// <summary>
+        /// What number should return for random numbers (1 is good choice...)
+        /// </summary>
+        public static int SetForcedRollValue(int value)
         {
             _ForcedRandomValue = value;
+            return _ForcedRandomValue;
         }
 
-        // Turn Random State Off
-        public static void DisableForcedRolls()
+        /// <summary>
+        /// Turn Random State Off
+        /// </summary>
+        /// <returns></returns>
+        public static bool DisableForcedRolls()
         {
             ForceRollsToNotRandom = false;
+            return ForceRollsToNotRandom;
         }
 
-        // Turn Random State On
+        /// <summary>
+        /// Turn Random State On
+        /// </summary>
         public static void EnableForcedRolls()
         {
             ForceRollsToNotRandom = true;
@@ -49,14 +61,16 @@ namespace Game.Helpers
                 return 0;
             }
 
-            if (dice < 1)
-            {
-                return 0;
-            }
-
+            // ForceRolls check after the rolls check, that prevents the bug of negative rolls
             if (ForceRollsToNotRandom)
             {
                 return rolls * _ForcedRandomValue;
+            }
+
+            // Dice check is after Rolls number check to allow for force roll value even if dice it 0
+            if (dice < 1)
+            {
+                return 0;
             }
 
             var myReturn = 0;
