dd3ce2219d360863d791a71e86afced280cba666
Phong Nguyen <nguyenphong@seattleu.edu>
2021-02-28 00:41:32 -0800

    Added CollectionView in PickItemsPage. Changed in PickItemsPage.xaml and PickItemsPage.xaml.cs

--- Game/Game/Views/Battle/PickItemsPage.xaml.cs
+++ Game/Game/Views/Battle/PickItemsPage.xaml.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.ComponentModel;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
+using Game.Models;
+using Game.ViewModels;
 
 namespace Game.Views
 {
@@ -10,12 +15,17 @@ namespace Game.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PickItemsPage : ContentPage
     {
+        // The view model, used for data binding
+        readonly ItemIndexViewModel ViewModel = ItemIndexViewModel.Instance;
+
         /// <summary>
         /// Constructor
         /// </summary>
         public PickItemsPage()
         {
             InitializeComponent();
+
+            BindingContext = ViewModel;
         }
         /// <summary>
         /// Quit the Battle
@@ -27,6 +37,54 @@ namespace Game.Views
         public async void CloseButton_Clicked(object sender, EventArgs e)
         {
             await Navigation.PopModalAsync();
+        }
+
+        #region CollectionView Handlers
+
+        /// <summary>
+        /// Select the item from the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public async void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs args)
+        {
+            ItemModel data = ItemsCollectionView.SelectedItem as ItemModel;
+            if (data == null)
+            {
+                return;
+            }
+
+            // Open the Read Page
+            await Navigation.PushAsync(new ItemReadPage(new GenericViewModel<ItemModel>(data)));
+
+            // Manually deselect item.
+            ItemsCollectionView.SelectedItem = null;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Refresh the list on page appearing
+        /// </summary>
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            BindingContext = null;
+
+            // If no data, then set it for needing refresh
+            if (ViewModel.Dataset.Count == 0)
+            {
+                ViewModel.SetNeedsRefresh(true);
+            }
+
+            // If the needs Refresh flag is set update it
+            if (ViewModel.NeedsRefresh())
+            {
+                ViewModel.LoadDatasetCommand.Execute(null);
+            }
+
+            BindingContext = ViewModel;
         }
     }
 }
