6987c453b087c3abf116c4f0292a490c63fd53eb
Phong Nguyen <nguyenphong@seattleu.edu>
2021-03-05 02:19:54 -0800

    Updated ExitButton Clicked leads to GamePage in BattlePage.xaml.cs

--- Game/Game/Views/Battle/BattlePage.xaml.cs
+++ Game/Game/Views/Battle/BattlePage.xaml.cs
@@ -771,7 +771,7 @@ namespace Game.Views
         /// <param name="e"></param>
         public async void ExitButton_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PopModalAsync();
+            await Navigation.PushModalAsync(new GamePage());
         }
 
         /// <summary>
