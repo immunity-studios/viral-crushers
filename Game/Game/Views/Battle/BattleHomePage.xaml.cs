4c0a23ad4bde98c66c0682438c992982d6003e2c
Ross Hoyt <43669429+rosshoyt@users.noreply.github.com>
2021-03-07 22:18:11 -0800

    Added comments to BattleHomePage.xaml.cs

--- Game/Game/Views/Battle/BattleHomePage.xaml.cs
+++ Game/Game/Views/Battle/BattleHomePage.xaml.cs
@@ -5,7 +5,8 @@ using Xamarin.Forms.Xaml;
 namespace Game.Views
 {
 	/// <summary>
-	/// The Main Game Page
+	/// This page is temporarily holding links to all battle pages for debugging/UI development
+	/// TODO delete this page and remove dungeon button from game page
 	/// </summary>
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class BattleHomePage : ContentPage
@@ -42,26 +43,41 @@ namespace Game.Views
 			await Navigation.PushAsync(new PickCharactersPage());
 		}
 
+		/// <summary>
+		/// Jump to the Show Monsters page
+		/// </summary>
 		public async void ShowMonstersPage_Clicked(object sender, EventArgs e)
 		{
 			await Navigation.PushAsync(new ShowMonstersPage());
 		}
 
+		/// <summary>
+		/// Jump to the Pick Items page 
+		/// </summary>
 		public async void PickItemsPage_Clicked(object sender, EventArgs e)
 		{
 			await Navigation.PushAsync(new PickItemsPage());
 		}
 
+		/// <summary>
+		/// Jump to the Round over page
+		/// </summary>
 		public async void RoundOverPage_Clicked(object sender, EventArgs e)
 		{
 			await Navigation.PushAsync(new RoundOverPage());
 		}
 
+		/// <summary>
+		/// Jump to the New Round page
+		/// </summary>
 		public async void NewRoundPage_Clicked(object sender, EventArgs e)
 		{
 			await Navigation.PushAsync(new NewRoundPage());
 		}
 
+		/// <summary>
+		/// Jump to the score page
+		/// </summary>
 		public async void ScorePage_Clicked(object sender, EventArgs e)
 		{
 			await Navigation.PushModalAsync(new GameOverPage());
