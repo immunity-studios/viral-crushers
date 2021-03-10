68f8c348bdab83337433084b27080bc64470de0d
Loi Pham <phaml5@seattleu.edu>
2021-03-10 11:51:19 -0800

    Updated battle buttons in BattlePage.xaml

--- Game/Game.Android/Resources/Resource.designer.cs
+++ Game/Game.Android/Resources/Resource.designer.cs
@@ -12401,139 +12401,181 @@ namespace Game.Droid
 			public const int icon_back_button_140 = 2131165351;
 			
 			// aapt resource value: 0x7F0700A8
-			public const int icon_black_dice = 2131165352;
+			public const int icon_battle_ability_button = 2131165352;
 			
 			// aapt resource value: 0x7F0700A9
-			public const int icon_blue_monster = 2131165353;
+			public const int icon_battle_attack_button = 2131165353;
 			
 			// aapt resource value: 0x7F0700AA
-			public const int icon_bug_spray = 2131165354;
+			public const int icon_battle_attack_sword = 2131165354;
 			
 			// aapt resource value: 0x7F0700AB
-			public const int icon_cancel = 2131165355;
+			public const int icon_battle_begin_button = 2131165355;
 			
 			// aapt resource value: 0x7F0700AC
-			public const int icon_character = 2131165356;
+			public const int icon_battle_close_button = 2131165356;
 			
 			// aapt resource value: 0x7F0700AD
-			public const int icon_delete = 2131165357;
+			public const int icon_battle_exit_button = 2131165357;
 			
 			// aapt resource value: 0x7F0700AE
-			public const int icon_doctor = 2131165358;
+			public const int icon_battle_move_button = 2131165358;
 			
 			// aapt resource value: 0x7F0700AF
-			public const int icon_edit = 2131165359;
+			public const int icon_battle_next_round_button = 2131165359;
 			
 			// aapt resource value: 0x7F0700B0
-			public const int icon_face_shield = 2131165360;
+			public const int icon_battle_start_button = 2131165360;
 			
 			// aapt resource value: 0x7F0700B1
-			public const int icon_firefighter = 2131165361;
+			public const int icon_black_dice = 2131165361;
 			
 			// aapt resource value: 0x7F0700B2
-			public const int icon_gate = 2131165362;
+			public const int icon_blue_monster = 2131165362;
 			
 			// aapt resource value: 0x7F0700B3
-			public const int icon_gift = 2131165363;
+			public const int icon_bug_spray = 2131165363;
 			
 			// aapt resource value: 0x7F0700B4
-			public const int icon_gloves = 2131165364;
+			public const int icon_cancel = 2131165364;
 			
 			// aapt resource value: 0x7F0700B5
-			public const int icon_goggles = 2131165365;
+			public const int icon_character = 2131165365;
 			
 			// aapt resource value: 0x7F0700B6
-			public const int icon_green_monster = 2131165366;
+			public const int icon_characters_bottom_menu = 2131165366;
 			
 			// aapt resource value: 0x7F0700B7
-			public const int icon_hand_soap = 2131165367;
+			public const int icon_defense_shield = 2131165367;
 			
 			// aapt resource value: 0x7F0700B8
-			public const int icon_hazmat_suit = 2131165368;
+			public const int icon_delete = 2131165368;
 			
 			// aapt resource value: 0x7F0700B9
-			public const int icon_hospital = 2131165369;
+			public const int icon_doctor = 2131165369;
 			
 			// aapt resource value: 0x7F0700BA
-			public const int icon_invisible_cloak = 2131165370;
+			public const int icon_edit = 2131165370;
 			
 			// aapt resource value: 0x7F0700BB
-			public const int icon_magenta_monster = 2131165371;
+			public const int icon_face_shield = 2131165371;
 			
 			// aapt resource value: 0x7F0700BC
-			public const int icon_mask = 2131165372;
+			public const int icon_firefighter = 2131165372;
 			
 			// aapt resource value: 0x7F0700BD
-			public const int icon_medicine = 2131165373;
+			public const int icon_gate = 2131165373;
 			
 			// aapt resource value: 0x7F0700BE
-			public const int icon_menu_characters_boy = 2131165374;
+			public const int icon_gift = 2131165374;
 			
 			// aapt resource value: 0x7F0700BF
-			public const int icon_menu_characters_girl = 2131165375;
+			public const int icon_gloves = 2131165375;
 			
 			// aapt resource value: 0x7F0700C0
-			public const int icon_menu_items = 2131165376;
+			public const int icon_goggles = 2131165376;
 			
 			// aapt resource value: 0x7F0700C1
-			public const int icon_menu_monsters = 2131165377;
+			public const int icon_green_monster = 2131165377;
 			
 			// aapt resource value: 0x7F0700C2
-			public const int icon_monster5 = 2131165378;
+			public const int icon_hand_soap = 2131165378;
 			
 			// aapt resource value: 0x7F0700C3
-			public const int icon_mosquito_net = 2131165379;
+			public const int icon_hazmat_suit = 2131165379;
 			
 			// aapt resource value: 0x7F0700C4
-			public const int icon_neon_green_monster = 2131165380;
+			public const int icon_health_red_cross = 2131165380;
 			
 			// aapt resource value: 0x7F0700C5
-			public const int icon_new = 2131165381;
+			public const int icon_hospital = 2131165381;
 			
 			// aapt resource value: 0x7F0700C6
-			public const int icon_officer = 2131165382;
+			public const int icon_invisible_cloak = 2131165382;
 			
 			// aapt resource value: 0x7F0700C7
-			public const int icon_orange_monster = 2131165383;
+			public const int icon_magenta_monster = 2131165383;
 			
 			// aapt resource value: 0x7F0700C8
-			public const int icon_panacea = 2131165384;
+			public const int icon_mask = 2131165384;
 			
 			// aapt resource value: 0x7F0700C9
-			public const int icon_play = 2131165385;
+			public const int icon_max_health_red_cross = 2131165385;
 			
 			// aapt resource value: 0x7F0700CA
-			public const int icon_purple_monster = 2131165386;
+			public const int icon_medicine = 2131165386;
 			
 			// aapt resource value: 0x7F0700CB
-			public const int icon_red_dice = 2131165387;
+			public const int icon_menu_characters_boy = 2131165387;
 			
 			// aapt resource value: 0x7F0700CC
-			public const int icon_red_monster = 2131165388;
+			public const int icon_menu_characters_girl = 2131165388;
 			
 			// aapt resource value: 0x7F0700CD
-			public const int icon_sanitizer = 2131165389;
+			public const int icon_menu_items = 2131165389;
 			
 			// aapt resource value: 0x7F0700CE
-			public const int icon_save = 2131165390;
+			public const int icon_menu_monsters = 2131165390;
 			
 			// aapt resource value: 0x7F0700CF
-			public const int icon_soup = 2131165391;
+			public const int icon_monster5 = 2131165391;
 			
 			// aapt resource value: 0x7F0700D0
-			public const int icon_student = 2131165392;
+			public const int icon_mosquito_net = 2131165392;
 			
 			// aapt resource value: 0x7F0700D1
-			public const int icon_teacher = 2131165393;
+			public const int icon_neon_green_monster = 2131165393;
 			
 			// aapt resource value: 0x7F0700D2
-			public const int icon_tile = 2131165394;
+			public const int icon_new = 2131165394;
 			
 			// aapt resource value: 0x7F0700D3
-			public const int icon_tissue_box = 2131165395;
+			public const int icon_officer = 2131165395;
 			
 			// aapt resource value: 0x7F0700D4
-			public const int icon_vaccine = 2131165396;
+			public const int icon_orange_monster = 2131165396;
+			
+			// aapt resource value: 0x7F0700D5
+			public const int icon_panacea = 2131165397;
+			
+			// aapt resource value: 0x7F0700D6
+			public const int icon_play = 2131165398;
+			
+			// aapt resource value: 0x7F0700D7
+			public const int icon_purple_monster = 2131165399;
+			
+			// aapt resource value: 0x7F0700D8
+			public const int icon_red_dice = 2131165400;
+			
+			// aapt resource value: 0x7F0700D9
+			public const int icon_red_monster = 2131165401;
+			
+			// aapt resource value: 0x7F0700DA
+			public const int icon_sanitizer = 2131165402;
+			
+			// aapt resource value: 0x7F0700DB
+			public const int icon_save = 2131165403;
+			
+			// aapt resource value: 0x7F0700DC
+			public const int icon_soup = 2131165404;
+			
+			// aapt resource value: 0x7F0700DD
+			public const int icon_speed = 2131165405;
+			
+			// aapt resource value: 0x7F0700DE
+			public const int icon_student = 2131165406;
+			
+			// aapt resource value: 0x7F0700DF
+			public const int icon_teacher = 2131165407;
+			
+			// aapt resource value: 0x7F0700E0
+			public const int icon_tile = 2131165408;
+			
+			// aapt resource value: 0x7F0700E1
+			public const int icon_tissue_box = 2131165409;
+			
+			// aapt resource value: 0x7F0700E2
+			public const int icon_vaccine = 2131165410;
 			
 			// aapt resource value: 0x7F07009F
 			public const int ic_mtrl_checked_circle = 2131165343;
@@ -12547,134 +12589,134 @@ namespace Game.Droid
 			// aapt resource value: 0x7F0700A2
 			public const int ic_mtrl_chip_close_circle = 2131165346;
 			
-			// aapt resource value: 0x7F0700D5
-			public const int item = 2131165397;
-			
-			// aapt resource value: 0x7F0700D6
-			public const int malaria_monster_140 = 2131165398;
-			
-			// aapt resource value: 0x7F0700D7
-			public const int mapcell = 2131165399;
-			
-			// aapt resource value: 0x7F0700D8
-			public const int material_ic_calendar_black_24dp = 2131165400;
-			
-			// aapt resource value: 0x7F0700D9
-			public const int material_ic_clear_black_24dp = 2131165401;
-			
-			// aapt resource value: 0x7F0700DA
-			public const int material_ic_edit_black_24dp = 2131165402;
-			
-			// aapt resource value: 0x7F0700DB
-			public const int material_ic_keyboard_arrow_left_black_24dp = 2131165403;
-			
-			// aapt resource value: 0x7F0700DC
-			public const int material_ic_keyboard_arrow_right_black_24dp = 2131165404;
-			
-			// aapt resource value: 0x7F0700DD
-			public const int material_ic_menu_arrow_down_black_24dp = 2131165405;
-			
-			// aapt resource value: 0x7F0700DE
-			public const int material_ic_menu_arrow_up_black_24dp = 2131165406;
-			
-			// aapt resource value: 0x7F0700DF
-			public const int measles_monster_140 = 2131165407;
-			
-			// aapt resource value: 0x7F0700E0
-			public const int menu_battle_icon_100 = 2131165408;
-			
-			// aapt resource value: 0x7F0700E1
-			public const int menu_items_icon_100 = 2131165409;
-			
-			// aapt resource value: 0x7F0700E2
-			public const int menu_monster_icon_100 = 2131165410;
-			
 			// aapt resource value: 0x7F0700E3
-			public const int menu_score_icon_100 = 2131165411;
+			public const int item = 2131165411;
 			
 			// aapt resource value: 0x7F0700E4
-			public const int mtrl_dialog_background = 2131165412;
+			public const int malaria_monster_140 = 2131165412;
 			
 			// aapt resource value: 0x7F0700E5
-			public const int mtrl_dropdown_arrow = 2131165413;
+			public const int mapcell = 2131165413;
 			
 			// aapt resource value: 0x7F0700E6
-			public const int mtrl_ic_arrow_drop_down = 2131165414;
+			public const int material_ic_calendar_black_24dp = 2131165414;
 			
 			// aapt resource value: 0x7F0700E7
-			public const int mtrl_ic_arrow_drop_up = 2131165415;
+			public const int material_ic_clear_black_24dp = 2131165415;
 			
 			// aapt resource value: 0x7F0700E8
-			public const int mtrl_ic_cancel = 2131165416;
+			public const int material_ic_edit_black_24dp = 2131165416;
 			
 			// aapt resource value: 0x7F0700E9
-			public const int mtrl_ic_error = 2131165417;
+			public const int material_ic_keyboard_arrow_left_black_24dp = 2131165417;
 			
 			// aapt resource value: 0x7F0700EA
-			public const int mtrl_popupmenu_background = 2131165418;
+			public const int material_ic_keyboard_arrow_right_black_24dp = 2131165418;
 			
 			// aapt resource value: 0x7F0700EB
-			public const int mtrl_popupmenu_background_dark = 2131165419;
+			public const int material_ic_menu_arrow_down_black_24dp = 2131165419;
 			
 			// aapt resource value: 0x7F0700EC
-			public const int mtrl_tabs_default_indicator = 2131165420;
+			public const int material_ic_menu_arrow_up_black_24dp = 2131165420;
 			
 			// aapt resource value: 0x7F0700ED
-			public const int navigation_empty_icon = 2131165421;
+			public const int measles_monster_140 = 2131165421;
 			
 			// aapt resource value: 0x7F0700EE
-			public const int notification_action_background = 2131165422;
+			public const int menu_battle_icon_100 = 2131165422;
 			
 			// aapt resource value: 0x7F0700EF
-			public const int notification_bg = 2131165423;
+			public const int menu_items_icon_100 = 2131165423;
 			
 			// aapt resource value: 0x7F0700F0
-			public const int notification_bg_low = 2131165424;
+			public const int menu_monster_icon_100 = 2131165424;
 			
 			// aapt resource value: 0x7F0700F1
-			public const int notification_bg_low_normal = 2131165425;
+			public const int menu_score_icon_100 = 2131165425;
 			
 			// aapt resource value: 0x7F0700F2
-			public const int notification_bg_low_pressed = 2131165426;
+			public const int mtrl_dialog_background = 2131165426;
 			
 			// aapt resource value: 0x7F0700F3
-			public const int notification_bg_normal = 2131165427;
+			public const int mtrl_dropdown_arrow = 2131165427;
 			
 			// aapt resource value: 0x7F0700F4
-			public const int notification_bg_normal_pressed = 2131165428;
+			public const int mtrl_ic_arrow_drop_down = 2131165428;
 			
 			// aapt resource value: 0x7F0700F5
-			public const int notification_icon_background = 2131165429;
+			public const int mtrl_ic_arrow_drop_up = 2131165429;
 			
 			// aapt resource value: 0x7F0700F6
-			public const int notification_template_icon_bg = 2131165430;
+			public const int mtrl_ic_cancel = 2131165430;
 			
 			// aapt resource value: 0x7F0700F7
-			public const int notification_template_icon_low_bg = 2131165431;
+			public const int mtrl_ic_error = 2131165431;
 			
 			// aapt resource value: 0x7F0700F8
-			public const int notification_tile_bg = 2131165432;
+			public const int mtrl_popupmenu_background = 2131165432;
 			
 			// aapt resource value: 0x7F0700F9
-			public const int notify_panel_notification_icon_bg = 2131165433;
+			public const int mtrl_popupmenu_background_dark = 2131165433;
 			
 			// aapt resource value: 0x7F0700FA
-			public const int save_button_140 = 2131165434;
+			public const int mtrl_tabs_default_indicator = 2131165434;
 			
 			// aapt resource value: 0x7F0700FB
-			public const int splashscreen = 2131165435;
+			public const int navigation_empty_icon = 2131165435;
 			
 			// aapt resource value: 0x7F0700FC
-			public const int test_custom_background = 2131165436;
+			public const int notification_action_background = 2131165436;
 			
 			// aapt resource value: 0x7F0700FD
-			public const int tooltip_frame_dark = 2131165437;
+			public const int notification_bg = 2131165437;
 			
 			// aapt resource value: 0x7F0700FE
-			public const int tooltip_frame_light = 2131165438;
+			public const int notification_bg_low = 2131165438;
 			
 			// aapt resource value: 0x7F0700FF
-			public const int vaccine = 2131165439;
+			public const int notification_bg_low_normal = 2131165439;
+			
+			// aapt resource value: 0x7F070100
+			public const int notification_bg_low_pressed = 2131165440;
+			
+			// aapt resource value: 0x7F070101
+			public const int notification_bg_normal = 2131165441;
+			
+			// aapt resource value: 0x7F070102
+			public const int notification_bg_normal_pressed = 2131165442;
+			
+			// aapt resource value: 0x7F070103
+			public const int notification_icon_background = 2131165443;
+			
+			// aapt resource value: 0x7F070104
+			public const int notification_template_icon_bg = 2131165444;
+			
+			// aapt resource value: 0x7F070105
+			public const int notification_template_icon_low_bg = 2131165445;
+			
+			// aapt resource value: 0x7F070106
+			public const int notification_tile_bg = 2131165446;
+			
+			// aapt resource value: 0x7F070107
+			public const int notify_panel_notification_icon_bg = 2131165447;
+			
+			// aapt resource value: 0x7F070108
+			public const int save_button_140 = 2131165448;
+			
+			// aapt resource value: 0x7F070109
+			public const int splashscreen = 2131165449;
+			
+			// aapt resource value: 0x7F07010A
+			public const int test_custom_background = 2131165450;
+			
+			// aapt resource value: 0x7F07010B
+			public const int tooltip_frame_dark = 2131165451;
+			
+			// aapt resource value: 0x7F07010C
+			public const int tooltip_frame_light = 2131165452;
+			
+			// aapt resource value: 0x7F07010D
+			public const int vaccine = 2131165453;
 			
 			static Drawable()
 			{
