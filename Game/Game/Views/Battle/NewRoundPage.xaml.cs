c6890e4306da7a2e01958386e3b449c0eed75541
Phong Nguyen <nguyenphong@seattleu.edu>
2021-03-13 23:34:16 -0800

    Updated CreatePlayerDisplayBox in NewRoundPage.xaml.cs

--- Game/Game/Views/Battle/NewRoundPage.xaml.cs
+++ Game/Game/Views/Battle/NewRoundPage.xaml.cs
@@ -64,14 +64,55 @@ namespace Game.Views
             // Hookup the image
             var PlayerImage = new Image
             {
-                Style = (Style)Application.Current.Resources["ImageBattleLargeStyle"],
+                Style = (Style)Application.Current.Resources["ImageBattleMediumStyle"],
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
                 Source = data.ImageURI
             };
+
+            // Hookup the background image
+            var CharacterBackgroundImage = new Image
+            {
+                Style = (Style)Application.Current.Resources["ImageBattleLargeStyle"],
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                Source = "character background.png"
+            };
+
+            // Hookup the background image
+            var MonsterBackgroundImage = new Image
+            {
+                Style = (Style)Application.Current.Resources["ImageBattleLargeStyle"],
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                Source = "monster background.png"
+            };
+
+            // Player Image Grid
+            var ImageGrid = new Grid()
+            {
+                RowDefinitions =
+            {
+                new RowDefinition()
+            }
+            };
+
+            // Row 0
+            if (data.PlayerType == PlayerTypeEnum.Character)
+            {
+                ImageGrid.Children.Add(CharacterBackgroundImage);
+            } else
+            {
+                ImageGrid.Children.Add(MonsterBackgroundImage);
+            }
+            
+            ImageGrid.Children.Add(PlayerImage);
 
             // Add the Level
             var PlayerLevelLabel = new Label
             {
-                Text = "Level : "+data.Level,
+                Text = "Level: "+data.Level,
+                TextColor = Color.DarkOrange,
                 Style = (Style)Application.Current.Resources["ValueStyleMicro"],
                 HorizontalOptions = LayoutOptions.Center,
                 HorizontalTextAlignment = TextAlignment.Center,
@@ -85,26 +126,46 @@ namespace Game.Views
             // Add the HP
             var PlayerHPLabel = new Label
             {
-                Text = "HP : "+ data.GetCurrentHealthTotal,
+                Text = "Hp:"+ data.GetCurrentHealthTotal,
+                TextColor = Color.DarkOrange,
                 Style = (Style)Application.Current.Resources["ValueStyleMicro"],
-                HorizontalOptions = LayoutOptions.Center,
-                HorizontalTextAlignment = TextAlignment.Center,
-                Padding = 0,
-                LineBreakMode = LineBreakMode.TailTruncation,
+                Padding = 1,
                 CharacterSpacing = 1,
                 LineHeight = 1,
                 MaxLines = 1,
             };
 
-            // Add the Range
-            var PlayerRangeLabel = new Label
+            // Add the Speed
+            var PlayerSpeedLabel = new Label
             {
-                Text = "Range : " + data.GetRange(),
+                Text = "Spd:" + data.Speed,
+                TextColor = Color.DarkOrange,
                 Style = (Style)Application.Current.Resources["ValueStyleMicro"],
-                HorizontalOptions = LayoutOptions.Center,
-                HorizontalTextAlignment = TextAlignment.Center,
-                Padding = 0,
-                LineBreakMode = LineBreakMode.TailTruncation,
+                Padding = 1,
+                CharacterSpacing = 1,
+                LineHeight = 1,
+                MaxLines = 1,
+            };
+
+            // Add the Attack
+            var PlayerAttackLabel = new Label
+            {
+                Text = "Atk:" + data.Attack,
+                TextColor = Color.DarkOrange,
+                Style = (Style)Application.Current.Resources["ValueStyleMicro"],
+                Padding = 1,
+                CharacterSpacing = 1,
+                LineHeight = 1,
+                MaxLines = 1,
+            };
+
+            // Add the Defense
+            var PlayerDefenseLabel = new Label
+            {
+                Text = "Def:" + data.Defense,
+                TextColor = Color.DarkOrange,
+                Style = (Style)Application.Current.Resources["ValueStyleMicro"],
+                Padding = 1,
                 CharacterSpacing = 1,
                 LineHeight = 1,
                 MaxLines = 1,
@@ -113,29 +174,50 @@ namespace Game.Views
             var PlayerNameLabel = new Label()
             {
                 Text = data.Name,
+                TextColor = Color.DarkOrange,
                 Style = (Style)Application.Current.Resources["ValueStyle"],
                 HorizontalOptions = LayoutOptions.Center,
                 HorizontalTextAlignment = TextAlignment.Center,
                 Padding = 0,
-                LineBreakMode = LineBreakMode.TailTruncation,
                 CharacterSpacing=1,
                 LineHeight=1,
                 MaxLines =1,
             };
+
+            // Put player information into Grid
+            var AttributeGrid = new Grid()
+            {
+                RowDefinitions =
+            {
+                new RowDefinition(),
+                new RowDefinition()
+            },
+                ColumnDefinitions =
+            {
+                new ColumnDefinition(),
+                new ColumnDefinition()
+            }
+            };
+
+            // Row 0
+            AttributeGrid.Children.Add(PlayerAttackLabel);
+            AttributeGrid.Children.Add(PlayerDefenseLabel, 0, 1);
+
+            // Row 1
+            AttributeGrid.Children.Add(PlayerHPLabel, 1, 0);
+            AttributeGrid.Children.Add(PlayerSpeedLabel, 1, 1);
 
             // Put the Image Button and Text inside a layout
             var PlayerStack = new StackLayout
             {
                 Style = (Style)Application.Current.Resources["PlayerInfoBox"],
                 HorizontalOptions = LayoutOptions.Center,
-                Padding = 0,
+                Padding = 2,
                 Spacing = 0,
                 Children = {
-                    PlayerImage,
                     PlayerNameLabel,
-                    PlayerLevelLabel,
-                    PlayerHPLabel,
-                    PlayerRangeLabel,
+                    ImageGrid,
+                    AttributeGrid,
                 },
             };
 
