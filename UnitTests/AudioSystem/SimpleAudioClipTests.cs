76a029e5c8d0abd8a8d097fbf6d3f57c2d7bdc92
Ross Hoyt <43669429+rosshoyt@users.noreply.github.com>
2021-03-17 23:57:21 -0700

    Added Constructor test for SimpleAudioClip, changed SimpleAudioClipTests.cs

--- UnitTests/AudioSystem/SimpleAudioClipTests.cs
+++ UnitTests/AudioSystem/SimpleAudioClipTests.cs
@@ -1,11 +1,30 @@
 ﻿using NUnit.Framework;
-
-using Game.Models;
+using Game.AudioSystem;
 
 namespace UnitTests.AudioSystem
 {
     [TestFixture]
     public class SimpleAudioClipTests
     {
+        SimpleAudioClip clip;
+
+        [TearDown]
+        public void TearDown()
+        {
+            clip = null;
+        }
+
+        [Test]
+        public void SimpleAudioClip_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+            clip = new SimpleAudioClip("fake/file/path");
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(clip);
+        }
+
+
     }
 }
