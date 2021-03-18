ab6f673d4480d2d2934fc801c81c5a4fbfa8cbbd
Phong Nguyen <nguyenphong@seattleu.edu>
2021-03-17 23:23:54 -0700

    Added BattlePage RunBattle Strong Monster Should GameOver in BattlePageScenerioTests.cs

--- UnitTests/Scenarios/BattlePageScenerioTests.cs
+++ UnitTests/Scenarios/BattlePageScenerioTests.cs
@@ -50,65 +50,78 @@ namespace Scenario
             Assert.IsNotNull(result);
         }
 
-        //[Test]
-        //public void BattlePage_RunBattle_Monsters_1_Should_Pass()
-        //{
-        //    //Arrange
+        [Test]
+        public void BattlePage_RunBattle_Strong_Monsters_Should_GameOver()
+        {
+            //Arrange
 
-        //    // Add Characters
+            // Add Characters
 
-        //    page.EngineViewModel.MaxNumberPartyCharacters = 1;
+            EngineViewModel.Engine.EngineSettings.MaxNumberPartyCharacters = 1;
 
-        //    var CharacterPlayerMike = new PlayerInfoModel(
-        //                    new CharacterModel
-        //                    {
-        //                        Speed = -1,
-        //                        Level = 10,
-        //                        CurrentHealth = 11,
-        //                        ExperienceTotal = 1,
-        //                        ExperienceRemaining = 1,
-        //                        Name = "Mike",
-        //                        ListOrder = 1,
-        //                    });
-
-        //    page.EngineViewModel.CharacterList.Add(CharacterPlayerMike);
-
-
-        //    // Add Monsters
-
-        //    // Need to set the Monster count to 1, so the battle goes to Next Round Faster
-        //    page.EngineViewModel.Engine.EngineSettings.MaxNumberPartyMonsters = 1;
+            var CharacterPlayerMike = new PlayerInfoModel(
+                            new CharacterModel
+                            {
+                                Speed = -1,
+                                Level = 1,
+                                CurrentHealth = 1,
+                                ExperienceTotal = 1,
+                                ExperienceRemaining = 1,
+                                Name = "Mike",
+                                ListOrder = 1,
+                            });
+            EngineViewModel.Engine.EngineSettings.CharacterList.Clear();
+            EngineViewModel.Engine.EngineSettings.CharacterList.Add(CharacterPlayerMike);
 
 
-        //    page.EngineViewModel.SetCurrentDefender(page.EngineViewModel.PlayerList.Where(m => m.PlayerType == PlayerTypeEnum.Monster).FirstOrDefault();
-        //    page.EngineViewModel.Engine.Round.SetCurrentAttacker(page.EngineViewModel.PlayerList.Where(m => m.PlayerType == PlayerTypeEnum.Character).FirstOrDefault();
+            // Add Monsters
+            // Need to set the Monster count to 1, so the battle goes to Next Round Faster
+            EngineViewModel.Engine.EngineSettings.MaxNumberPartyMonsters = 1;
 
-        //    //Act
-        //    RoundEnum result;
+            var MonsterPlayer = new PlayerInfoModel(
+                            new MonsterModel
+                            {
+                                Speed = 10,
+                                Level = 10,
+                                CurrentHealth = 100,
+                                ExperienceTotal = 1,
+                                ExperienceRemaining = 1,
+                                Attack = 100,
+                                Name = "Bubble",
+                                ListOrder = 1,
+                            });
+            EngineViewModel.Engine.EngineSettings.MonsterList.Clear();
+            EngineViewModel.Engine.EngineSettings.MonsterList.Add(MonsterPlayer);
 
-        //    // First Character Hits
-        //    page.AttackButton_Clicked(null, null);
-        //    result = page.EngineViewModel.Engine.RoundStateEnum;
-        //    Assert.AreEqual(RoundEnum.NextTurn, result);
+            // Make the List
+            EngineViewModel.Engine.EngineSettings.PlayerList = EngineViewModel.Engine.Round.MakePlayerList();
 
-        //    // Monsters Turn
-        //    page.AttackButton_Clicked(null, null);
-        //    result = page.EngineViewModel.Engine.RoundStateEnum;
-        //    Assert.AreEqual(RoundEnum.NextTurn, result);
+            // Set currentAttacker and CurrentDefender
+            EngineViewModel.Engine.Round.SetCurrentAttacker(EngineViewModel.Engine.EngineSettings.PlayerList.Where(m => m.PlayerType == PlayerTypeEnum.Monster).FirstOrDefault());
+            //EngineViewModel.Engine.Round.SetCurrentDefender(EngineViewModel.Engine.EngineSettings.PlayerList.Where(m => m.PlayerType == PlayerTypeEnum.Character).FirstOrDefault());
 
-        //    //// loop until game over
-        //    //do
-        //    //{
-        //    //    page.AttackButton_Clicked(null, null);
-        //    //    result = page.EngineViewModel.Engine.RoundStateEnum;
-        //    //    Assert.AreEqual(RoundEnum.NextTurn, result);
-        //    //}
-        //    //while (result != RoundEnum.GameOver);
+            //Act
+            RoundEnum result;
 
-        //    //Reset
+            // Force a Hit
+            DiceHelper.EnableForcedRolls();
+            DiceHelper.SetForcedRollValue(20);
 
-        //    //Assert
-        //    Assert.AreEqual(true, true);
-        //}
+            // First Monster Hits
+            EngineViewModel.Engine.EngineSettings.CurrentAction = ActionEnum.Attack;
+            page.NextMonsterAttack();
+            result = EngineViewModel.Engine.EngineSettings.RoundStateEnum;
+
+            // Monsters Turn
+            page.AttackButton_Clicked(null, null);
+            result = EngineViewModel.Engine.EngineSettings.RoundStateEnum;
+
+
+            //Reset
+            DiceHelper.DisableForcedRolls();
+
+            //Assert
+            Assert.AreEqual(RoundEnum.GameOver, result);
+        }
     }
 }
