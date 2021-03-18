6c8f906e76609d32da750563d8e11dae60fab8d5
Ross Hoyt <43669429+rosshoyt@users.noreply.github.com>
2021-03-17 18:14:05 -0700

    Added setup method to AudioEngineTests.cs

--- UnitTests/AudioSystem/AudioEngineTests.cs
+++ UnitTests/AudioSystem/AudioEngineTests.cs
@@ -1,9 +1,17 @@
 ﻿using NUnit.Framework;
+using Game.AudioSystem;
 
 namespace UnitTests.AudioSystem
 {
     [TestFixture]
     public class AudioEngineTests
     {
+        AudioEngine audioEngine;
+
+        [SetUp]
+        public void Setup()
+        {
+            audioEngine = new AudioEngine();
+        }
     }
 }
